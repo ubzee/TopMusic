5014af76ba90fc92ab756703889ac52f4c9950d9
Ubzee <ubzee@outlook.com>
2019-08-16 11:27:06 +0200

    Ajoutez des fichiers projet.

---
+++ TopMusicExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TopMusicExam.Startup))]
+namespace TopMusicExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
